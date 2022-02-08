35d3a1b589c36929031811477052b60c11683560
italo Martins Tomaz Rocha <082170012@ftt.cefsa.edu.br>
2022-02-08 15:52:50 -0300

    modelo tamplate com detecção de imagens funcionando

---
+++ libras4All_2/utilities.js
@@ -0,0 +1,31 @@
+// Define our labelmap
+const labelMap = {
+    1:{name:'hanglose', color:'red'},
+    2:{name:'joia', color:'yellow'},
+    3:{name:'nice', color:'lime'},
+    4:{name:'livelong', color:'blue'},
+}
+
+// Define a drawing function
+export const drawRect = (boxes, classes, scores, threshold, imgWidth, imgHeight, ctx)=>{
+    console.log('entrou no draw React');
+    for(let i=0; i<=boxes.length; i++){
+        if(boxes[i] && classes[i] && scores[i]>threshold){
+            // Extract variables
+            const [y,x,height,width] = boxes[i]
+            const text = classes[i]
+            
+            // Set styling
+            ctx.strokeStyle = labelMap[text]['color']
+            ctx.lineWidth = 10
+            ctx.fillStyle = 'black'
+            ctx.font = '30px Arial'         
+            
+            // DRAW!!
+            ctx.beginPath()
+            ctx.fillText(labelMap[text]['name'] + ' - ' + Math.round(scores[i]*100)/100, x*imgWidth, y*imgHeight-10)
+            ctx.rect(x*imgWidth, y*imgHeight, width*imgWidth/2, height*imgHeight/2);
+            ctx.stroke()
+        }
+    }
+}
