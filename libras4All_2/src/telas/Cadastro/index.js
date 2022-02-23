385cec70227ffedaf55694c822e62051fe9aa981
Gabriella Bittencourt <ggbittencourtcampos@gmail.com>
2022-02-22 22:53:23 -0300

    tela InserirPin

--- libras4All_2/src/telas/Cadastro/index.js
+++ libras4All_2/src/telas/Cadastro/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import {Text,View, TextInput, TouchableOpacity, SafeAreaView, StatusBar, Image} from 'react-native';
 import estilos from './estilos';
 
 export default function Cadastro() {
+    const [userNome, setUserNome] = useState('');
+    const [userEmail, setUserEmail] = useState('');
+    const [password, setPassword] = useState('');
     return <>
         <SafeAreaView style={estilos.fundo}>
             <StatusBar backgroundColor="rgb(35, 36, 95)"/>
@@ -16,7 +19,8 @@ export default function Cadastro() {
                 style={estilos.cadastro__input}
                 placeholder="Nome"
                 placeholderTextColor="#acacac" 
-                
+                onChangeText={novoNome => setUserNome(novoNome)}
+                defaultValue={userNome}
                 />
                 </View>
             <View style={estilos.icon_area}>
@@ -25,7 +29,8 @@ export default function Cadastro() {
                 style={estilos.cadastro__input}
                 placeholder="Email"
                 placeholderTextColor="#acacac" 
-               
+                onChangeText={novoEmail => setUserEmail(novoEmail)}
+                defaultValue={userEmail}
                 />
                 </View>
                 <View style={estilos.icon_area}>
@@ -35,7 +40,8 @@ export default function Cadastro() {
                 style={estilos.cadastro__input}
                 placeholder='Senha'
                 placeholderTextColor="#acacac" 
-                
+                onChangeText={novoPassword => setPassword(novoPassword)}
+                defaultValue={password}
                 secureTextEntry={true}/>
                 </View>
             <View style={estilos.botao}>               
@@ -46,4 +52,24 @@ export default function Cadastro() {
    
         </SafeAreaView>
     </>
+}
+const Cadastrar = (userNome,userEmail,password) => {
+    const corpo = JSON.stringify({"nome":userNome,"email":userEmail,"senha":password});
+    console.log(corpo);
+    fetch('https://libras4all.herokuapp.com/api/usuario/cadastro',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({"nome":userNome,"email":userEmail,"senha":password})
+    })
+    .then(response => response.json())
+    .then(responseJson => {
+        console.log('deu certo');
+        console.log(responseJson);
+    })
+    .catch(error => {
+      console.log('deu errado');
+      console.error(error);
+    });
 }
