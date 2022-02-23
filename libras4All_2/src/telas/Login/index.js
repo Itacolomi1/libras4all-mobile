e4dc2eb3e0e7cc80db3aa3ed10076c25b28dfd0a
italo Martins Tomaz Rocha <082170012@ftt.cefsa.edu.br>
2022-02-23 14:59:24 -0300

    feat: login funcionando

--- libras4All_2/src/telas/Login/index.js
+++ libras4All_2/src/telas/Login/index.js
@@ -1,11 +1,34 @@
-import React, {useState,useEffect} from 'react';
+import React, {useState} from 'react';
 import {Text,View, TextInput, TouchableOpacity, SafeAreaView, StatusBar, Image} from 'react-native';
-import Botao from '../../componentes/Botao'
+import * as settings from '../../assets/config/appSettings.json'
 import estilos from './estilos';
 
 export default function Login() {
     const [userEmail, setUserEmail] = useState('');
     const [password, setPassword] = useState('');
+
+
+    const Logar = () => {
+ 
+        const corpo = JSON.stringify({"email":userEmail,"senha":password});
+        console.log(corpo);
+        fetch( settings.backend.url + '/usuario/login',{
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({"email":userEmail,"senha":password})
+        })
+        .then(response => response.json())
+        .then(responseJson => {
+            console.log('deu certo');
+            console.log(responseJson);
+        })
+        .catch(error => {
+          console.log('deu errado');
+          console.error(error);
+        });
+    }
 
 
 
@@ -38,7 +61,10 @@ export default function Login() {
                 secureTextEntry={true}/>
                 </View>
             <View style={estilos.botao}>
-                <TouchableOpacity  style={estilos.login_button}>
+                <TouchableOpacity  
+                style={estilos.login_button}
+                onPress={Logar}
+                >
                     <Text style={estilos.texto_button}>Logar</Text>
                 </TouchableOpacity> 
                 <TouchableOpacity  style={estilos.cadastro_button}>
@@ -50,23 +76,3 @@ export default function Login() {
     </>
 }
 
-const Logar = (userEmail,password) => {
-    const corpo = JSON.stringify({"email":userEmail,"senha":password});
-    console.log(corpo);
-    fetch('https://libras4all.herokuapp.com/api/usuario/login',{
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({"email":userEmail,"senha":password})
-    })
-    .then(response => response.json())
-    .then(responseJson => {
-        console.log('deu certo');
-        console.log(responseJson);
-    })
-    .catch(error => {
-      console.log('deu errado');
-      console.error(error);
-    });
-}
