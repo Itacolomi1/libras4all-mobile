19f267d11700072a5827cf4d7cc46371f7b24a19
italo Martins Tomaz Rocha <082170012@ftt.cefsa.edu.br>
2022-02-23 16:14:12 -0300

    feat: Inserir Pin

--- libras4All_2/src/telas/InserirPin/index.js
+++ libras4All_2/src/telas/InserirPin/index.js
@@ -1,13 +1,38 @@
-import React, {useState,useEffect} from 'react';
+import React, {useState} from 'react';
 import {Text,View, Dimensions, TextInput, TouchableOpacity, SafeAreaView, StatusBar, Image} from 'react-native';
-import Botao from '../../componentes/Botao'
 import estilos from './estilos';
+import * as settings from '../../assets/config/appSettings.json'
 
 const dimensions = Dimensions.get('window');
 const imageHeight = Math.round(dimensions.width * 9 / 17);
 const imageWidth = dimensions.width;
 
 export default function InserirPin() {
+
+    const [pin,setPin] = useState('0');
+    const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMDk3MTNkY2U2NDQyMDAxNjhhZDk5MCIsImlhdCI6MTY0NTY0MTc5M30.-nV2_60oHvjkj_GhGrqOQKwRT-q7vhDpys3S5fJPjks';
+    const salaId = '6205b1a6b80a183bb4d1ba61';
+    
+    const ValidarPin = () => {   
+
+        fetch( settings.backend.url + `/sala/validarCodigo/${salaId}/${pin}`,{
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json; charset=utf-8',
+                'Authorization': 'Bearer ' + token
+            },    
+           
+        })
+        .then(response => response.json())
+        .then(responsejson =>{
+            console.log('Deu certo');
+            console.log(responsejson);
+        })
+        .catch(error => {
+          console.log('deu errado');
+          console.error(error);
+        });
+    }
     return <>
          <SafeAreaView style={estilos.fundo}>
             <StatusBar backgroundColor="rgb(35, 36, 95)"/>
@@ -20,12 +45,18 @@ export default function InserirPin() {
             <TextInput 
                 style={estilos.input}
                 placeholder="Insira o Pin"
-                placeholderTextColor="#acacac" 
+                placeholderTextColor="#acacac"
+                onChangeText={novoPin => setPin(novoPin)}
+                defaultValue={pin}
+                keyboardType='number-pad'
                 />
                 </View>
                 
             <View style={estilos.botao}>
-                <TouchableOpacity  style={estilos.button}>
+                <TouchableOpacity  
+                    style={estilos.button}
+                    onPress={ValidarPin}    
+                        >
                     <Text style={estilos.texto_button}>Enviar</Text>
                 </TouchableOpacity>           
             </View>
@@ -34,23 +65,3 @@ export default function InserirPin() {
     </>
 }
 
-const Logar = (userEmail,password) => {
-    const corpo = JSON.stringify({"email":userEmail,"senha":password});
-    console.log(corpo);
-    fetch('https://libras4all.herokuapp.com/api/usuario/login',{
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({"email":userEmail,"senha":password})
-    })
-    .then(response => response.json())
-    .then(responseJson => {
-        console.log('deu certo');
-        console.log(responseJson);
-    })
-    .catch(error => {
-      console.log('deu errado');
-      console.error(error);
-    });
-}
