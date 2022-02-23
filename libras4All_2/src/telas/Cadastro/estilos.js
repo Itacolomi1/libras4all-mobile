6634a4b789a1b0e250e60599a03d60b2e7aa8ef7
Gabriella Bittencourt <ggbittencourtcampos@gmail.com>
2022-02-22 21:27:43 -0300

    tela cadastro

---
+++ libras4All_2/src/telas/Cadastro/estilos.js
@@ -0,0 +1,82 @@
+import { AutoFocus } from "expo-camera/build/Camera.types";
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    fundo:{
+        backgroundColor:"white",
+        height: "100%",
+        justifyContent: "center",
+        color: "black"
+    },
+    
+    cadastro__input:{
+        fontSize: 15,       
+        color: "black",
+        width: "80%",
+        alignSelf: "center",
+        height: 50
+        
+    },
+    icon_area:{
+        width: "80%",        
+        alignSelf: "center",
+    flexDirection: 'row',
+    borderWidth: 1,
+        borderColor: "#acacac",
+        borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 50,
+    borderRadius: 5 ,
+    marginBottom: 30
+    },
+    input_icon:{
+        padding: 10,
+    margin: 5,
+    height: 25,
+    width: 25,
+    resizeMode : 'stretch',
+    alignItems: 'center'
+    },
+    texto_button:{
+        color: "#fff",
+        fontSize: 15,
+        alignSelf: "center",
+        
+        
+    },
+    salvar_button:{
+        justifyContent: 'center',
+        backgroundColor: "rgb(35, 36, 95)",
+        alignSelf:"center",
+        borderRadius: 5,
+        width: "80%",
+        alignSelf: "center",
+        marginBottom: 30,
+        height: 50,
+
+    },
+    Titulo:{
+        fontWeight:'bold',        
+        fontSize: 22,    
+        alignSelf: "center", 
+        backgroundColor: "white",        
+        color: "black",
+        alignItems: 'center',
+        marginBottom: 30
+        
+                  
+    },
+    engloba:{
+        width: "100%",
+        backgroundColor: "white",
+        
+        
+    },
+    botao:{
+        marginBottom: 5,
+        width: "100%",
+        alignItems: "center",
+        alignSelf: "center"
+    }
+});
--- libras4All_2/src/telas/Cadastro/index.js
+++ libras4All_2/src/telas/Cadastro/index.js
@@ -1,8 +1,49 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text,View, TextInput, TouchableOpacity, SafeAreaView, StatusBar, Image} from 'react-native';
+import estilos from './estilos';
 
 export default function Cadastro() {
     return <>
-        <Text>Tela Cadastro</Text>
+        <SafeAreaView style={estilos.fundo}>
+            <StatusBar backgroundColor="rgb(35, 36, 95)"/>
+        
+            <View style={estilos.engloba}>
+                <Text style={estilos.Titulo}>CADASTRO</Text>
+            </View>
+            <View style={estilos.icon_area}>
+            <Image source={require('../Images/user.png')} style={estilos.input_icon} />
+            <TextInput 
+                style={estilos.cadastro__input}
+                placeholder="Nome"
+                placeholderTextColor="#acacac" 
+                
+                />
+                </View>
+            <View style={estilos.icon_area}>
+            <Image source={require('../Images/email.png')} style={estilos.input_icon} />
+            <TextInput 
+                style={estilos.cadastro__input}
+                placeholder="Email"
+                placeholderTextColor="#acacac" 
+               
+                />
+                </View>
+                <View style={estilos.icon_area}>
+                <Image source={require('../Images/cadeado.png')} style={estilos.input_icon} />
+           
+            <TextInput 
+                style={estilos.cadastro__input}
+                placeholder='Senha'
+                placeholderTextColor="#acacac" 
+                
+                secureTextEntry={true}/>
+                </View>
+            <View style={estilos.botao}>               
+                <TouchableOpacity  style={estilos.salvar_button}>
+                    <Text style={estilos.texto_button}>Salvar</Text>
+                </TouchableOpacity>           
+            </View>
+   
+        </SafeAreaView>
     </>
 }
