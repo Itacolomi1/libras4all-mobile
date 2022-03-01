e856f37fdbe21d922dafdcd34762c24e797541d0
italo Martins Tomaz Rocha <082170012@ftt.cefsa.edu.br>
2022-03-01 13:16:06 -0300

    finalizando lógica Quiz

--- libras4All_2/src/telas/Quiz/index.js
+++ libras4All_2/src/telas/Quiz/index.js
@@ -5,14 +5,18 @@ import * as settings from '../../assets/config/appSettings.json'
 import {adicionaHistorico} from '../../services/historic.service';
 
 
+
+
 export default function Quiz({ navigation }) {
     const salaID = '621bf0052d53a30016a0b571';
     const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMWJmMTkzMmQ1M2EzMDAxNmEwYjU3ZSIsImlhdCI6MTY0NjA4MDc4NH0.2Vhsn6B1o6lJPlIS4MCdJrwwQo3hS67Rhuw9BOJBfns';
     const userId = '621bf1932d53a30016a0b57e';
     let perguntasID = [];
     let perguntasQuiz = [];
-    const [loading, setLoading] = useState(true);
-    const [pergunta, setPergunta] = useState(null);
+    const [loading, setLoading] = useState(true);    
+    const [listaPergunta,setListaPergunta] = useState([]);
+    const [perguntaDaVez,setPerguntaDaVez] = useState(0);
+    
 
 
     React.useEffect(() => {
@@ -57,8 +61,9 @@ export default function Quiz({ navigation }) {
             const element = perguntasID[index];
             let pergunta = await getPergunta(element);
             perguntasQuiz.push(pergunta);
-        }       
-        proximaPergunta();
+        }
+        setListaPergunta(perguntasQuiz);
+        //proximaPergunta();
         setLoading(false);
     }
 
@@ -84,29 +89,38 @@ export default function Quiz({ navigation }) {
         return retorno;
     }
 
-    function proximaPergunta(){
-        console.log('antes');
-        console.log(perguntasQuiz);
-        if(perguntasQuiz.length === 0){
+    function proximaPergunta(){   
+        
+
+        // console.log('lista de perguntas antes');
+        // console.log(perguntasQuiz);
+
+        // if(perguntasQuiz.length === 0){
+        //     Alert.alert('O Jogo Acabou');
+        //     return;
+        // }      
+        
+        // let perguntaTemp = perguntasQuiz.shift();
+        // console.log('A pergunta');
+        // console.log(perguntaTemp);
+        // setPergunta(perguntaTemp);
+        // console.log('lista de perguntas depois');
+        // console.log(perguntasQuiz);
+        let tempNumb = perguntaDaVez;
+        if((tempNumb + 1) >= listaPergunta.length){
             Alert.alert('O Jogo Acabou');
             return;
-        }        
-        
-        let perguntaTemp = perguntasQuiz.shift();
-        console.log('A pergunta');
-        console.log(perguntaTemp);
-        setPergunta(perguntaTemp);
-        console.log('depois');
-        console.log(perguntasQuiz);
+        }
+        setPerguntaDaVez(perguntaDaVez + 1); 
     }
 
     function validaResposta(alternativa){
        
         if(alternativa.perguntaCorreta === 'true'){
-            //adicionaHistorico(token,salaID,userId,'Quiz',pergunta._id,'true');
+            adicionaHistorico(token,salaID,userId,'Quiz',listaPergunta[perguntaDaVez]._id,'true');
             console.log('Acertou');
         }else{
-            //adicionaHistorico(token,salaID,userId,'Quiz',pergunta._id,'false');
+            adicionaHistorico(token,salaID,userId,'Quiz',listaPergunta[perguntaDaVez]._id,'false');
             console.log('Errou');
         }
         proximaPergunta();
@@ -121,32 +135,32 @@ export default function Quiz({ navigation }) {
 
                 <View style={estilos.topo}>
                     <Image source={require('../Images/categoria.png')} style={estilos.icon_categotia} />
-                    <Text style={estilos.titulo}>{pergunta.classe}</Text>
+                    <Text style={estilos.titulo}>{listaPergunta[perguntaDaVez].classe}</Text>
                     <View style={estilos.tempo} >
                         <Image source={require('../Images/fundo-tempo.png')} style={estilos.icon_categotia} />
                         <Text style={estilos.relogio}>20</Text>
                     </View>
                 </View>
                 <View style={[estilos.pergunta, estilos.elevation]}>
-                    <Text style={estilos.texto_pergunta}>{pergunta.descricao}</Text>
+                    <Text style={estilos.texto_pergunta}>{listaPergunta[perguntaDaVez].descricao}</Text>
                     <Image source={require('../Images/A.gif')} style={estilos.img_pergunta} />
 
                 </View>
                 <View style={estilos.alternativas}>
                     <TouchableOpacity 
                         style={estilos.alternativa_button}
-                        onPress={()=> {validaResposta(pergunta.alternativas[0])}} >
-                        <Text style={estilos.texto_button}>{pergunta.alternativas[0].descricao}</Text>
+                        onPress={()=> {validaResposta(listaPergunta[perguntaDaVez].alternativas[0])}} >
+                        <Text style={estilos.texto_button}>{listaPergunta[perguntaDaVez].alternativas[0].descricao}</Text>
                     </TouchableOpacity>
                     <TouchableOpacity 
                         style={estilos.alternativa_button}
-                        onPress={()=>{validaResposta(pergunta.alternativas[1])}}>
-                        <Text style={estilos.texto_button}>{pergunta.alternativas[1].descricao}</Text>
+                        onPress={()=>{validaResposta(listaPergunta[perguntaDaVez].alternativas[1])}}>
+                        <Text style={estilos.texto_button}>{listaPergunta[perguntaDaVez].alternativas[1].descricao}</Text>
                     </TouchableOpacity>
                     <TouchableOpacity 
                         style={estilos.alternativa_button}
-                        onPress={()=>{validaResposta(pergunta.alternativas[2])}}>
-                        <Text style={estilos.texto_button}>{pergunta.alternativas[2].descricao}</Text>
+                        onPress={()=>{validaResposta(listaPergunta[perguntaDaVez].alternativas[2])}}>
+                        <Text style={estilos.texto_button}>{listaPergunta[perguntaDaVez].alternativas[2].descricao}</Text>
                     </TouchableOpacity>
                 </View>
 
