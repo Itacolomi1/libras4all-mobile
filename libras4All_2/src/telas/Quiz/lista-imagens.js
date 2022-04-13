7a04540a4366b560d0016232967f136d4a5c13b1
Gabriella Bittencourt <ggbittencourtcampos@gmail.com>
2022-04-13 20:31:29 -0300

    imagem quiz

--- libras4All_2/src/telas/Quiz/lista-imagens.js
+++ libras4All_2/src/telas/Quiz/lista-imagens.js
@@ -3,67 +3,75 @@ export function listaImagens(){
     return [
         {
             id: '621bed41daa6bb2d9096f877',
-            image: require('../../assets/images/quiz/621bed41daa6bb2d9096f877.jpeg'),
+            image: require('../../assets/images/quiz/A.jpeg'),
         },
         {
             id: '621bed60daa6bb2d9096f878',
-            image: require('../../assets/images/quiz/621bed60daa6bb2d9096f878.jpeg'),
+            image: require('../../assets/images/quiz/B.jpeg'),
         },
         {
             id: '621bed72daa6bb2d9096f879',
-            image: require('../../assets/images/quiz/621bed72daa6bb2d9096f879.jpeg'),
+            image: require('../../assets/images/quiz/C.jpeg'),
         },
         {
             id: '621bed8cdaa6bb2d9096f87a',
-            image: require('../../assets/images/quiz/621bed8cdaa6bb2d9096f87a.jpeg'),
-        },
-        {
-            id: '621bedcbdaa6bb2d9096f87b',
-            image: require('../../assets/images/quiz/621bedcbdaa6bb2d9096f87b.jpeg'),
+            image: require('../../assets/images/quiz/D.jpeg'),
         },
         {
             id: '621beddddaa6bb2d9096f87c',
-            image: require('../../assets/images/quiz/621beddddaa6bb2d9096f87c.jpeg'),
+            image: require('../../assets/images/quiz/2.jpeg'),
         },
         {
             id: '621bee48daa6bb2d9096f87d',
-            image: require('../../assets/images/quiz/621bee48daa6bb2d9096f87d.jpeg'),
+            image: require('../../assets/images/quiz/3.jpeg'),
         },
         {
             id: '621bee50daa6bb2d9096f87e',
-            image: require('../../assets/images/quiz/621bee50daa6bb2d9096f87e.jpeg'),
+            image: require('../../assets/images/quiz/4.jpeg'),
         },
         {
             id: '621bee8fdaa6bb2d9096f87f',
-            image: require('../../assets/images/quiz/621bee8fdaa6bb2d9096f87f.gif'),
+            image: require('../../assets/images/quiz/Bom-dia.gif'),
         },
         {
             id: '621beea2daa6bb2d9096f880',
-            image: require('../../assets/images/quiz/621beea2daa6bb2d9096f880.gif'),
+            image: require('../../assets/images/quiz/Boa-tarde.gif'),
         },
         {
             id: '621beeb2daa6bb2d9096f881',
-            image: require('../../assets/images/quiz/621beeb2daa6bb2d9096f881.gif'),
+            image: require('../../assets/images/quiz/Boa-noite.gif'),
         },
         {
             id: '62229f587207f2001691aed4',
-            image: require('../../assets/images/quiz/62229f587207f2001691aed4.gif'),
+            image: require('../../assets/images/quiz/adriano.gif'),
         },
         {
             id: '62229fe27207f2001691aed5',
-            image: require('../../assets/images/quiz/62229fe27207f2001691aed5.gif'),
+            image: require('../../assets/images/quiz/luana.gif'),
         },
         {
             id: '6222a02d7207f2001691aed6',
-            image: require('../../assets/images/quiz/6222a02d7207f2001691aed6.gif'),
+            image: require('../../assets/images/quiz/viviane.gif'),
         },
         {
             id: '6222a0ab7207f2001691aed7',
-            image: require('../../assets/images/quiz/6222a0ab7207f2001691aed7.jpeg'),
+            image: require('../../assets/images/quiz/5.jpeg'),
         },
         {
-            id: '6222a0f97207f2001691aed8',
-            image: require('../../assets/images/quiz/6222a0f97207f2001691aed8.jpeg'),
+            id: '624905ad5c0c760016d923cb',
+            image: require('../../assets/images/quiz/M.jpeg'),
+        },
+        {
+            id: '624905d95c0c760016d923cc',
+            image: require('../../assets/images/quiz/P.jpeg'),
+        },
+        {
+            id: '624905f65c0c760016d923cd',
+            image: require('../../assets/images/quiz/R.jpeg'),
+        },
+        {
+            id: '624906135c0c760016d923ce',
+            image: require('../../assets/images/quiz/L.jpeg'),
         },
     ]
 }
