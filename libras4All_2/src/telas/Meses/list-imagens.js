6090824635b8a5c6d71fce8949def551ce42abab
milenaff <mih.ferreiradepaula@gmail.com>
2022-05-01 14:20:15 -0300

    Tela de aprendizado finalizada com as imagens

--- libras4All_2/src/telas/Meses/list-imagens.js
+++ libras4All_2/src/telas/Meses/list-imagens.js
@@ -5,50 +5,50 @@ export function listaImagens(){
             id: 'jan',
             image: require('../../assets/images/quiz/jan.gif'),
         },
-        // {
-        //     id: 'fev',
-        //     image: require('../../assets/images/quiz/fev.gif'),
-        // },
-        // {
-        //     id: 'mar',
-        //     image: require('../../assets/images/quiz/mar.gif'),
-        // },
-        // {
-        //     id: 'abr',
-        //     image: require('../../assets/images/quiz/abr.gif'),
-        // },
-        // {
-        //     id: 'mai',
-        //     image: require('../../assets/images/quiz/mai.gif'),
-        // },
-        // {
-        //     id: 'jun',
-        //     image: require('../../assets/images/quiz/jun.gif'),
-        // },
-        // {
-        //     id: 'jul',
-        //     image: require('../../assets/images/quiz/jul.gif'),
-        // },
-        // {
-        //     id: 'ago',
-        //     image: require('../../assets/images/quiz/ago.gif'),
-        // },
-        // {
-        //     id: 'set',
-        //     image: require('../../assets/images/quiz/set.gif'),
-        // },
-        // {
-        //     id: 'out',
-        //     image: require('../../assets/images/quiz/out.gif'),
-        // },
-        // {
-        //     id: 'nov',
-        //     image: require('../../assets/images/quiz/nov.gif'),
-        // },
-        // {
-        //     id: 'dez',
-        //     image: require('../../assets/images/quiz/dez.gif'),
-        // },
+        {
+            id: 'fev',
+            image: require('../../assets/images/quiz/fev.gif'),
+        },
+        {
+            id: 'mar',
+            image: require('../../assets/images/quiz/mar.gif'),
+        },
+        {
+            id: 'abr',
+            image: require('../../assets/images/quiz/abr.gif'),
+        },
+        {
+            id: 'mai',
+            image: require('../../assets/images/quiz/mai.gif'),
+        },
+        {
+            id: 'jun',
+            image: require('../../assets/images/quiz/jun.gif'),
+        },
+        {
+            id: 'jul',
+            image: require('../../assets/images/quiz/jul.gif'),
+        },
+        {
+            id: 'ago',
+            image: require('../../assets/images/quiz/ago.gif'),
+        },
+        {
+            id: 'set',
+            image: require('../../assets/images/quiz/set.gif'),
+        },
+        {
+            id: 'out',
+            image: require('../../assets/images/quiz/out.gif'),
+        },
+        {
+            id: 'nov',
+            image: require('../../assets/images/quiz/nov.gif'),
+        },
+        {
+            id: 'dez',
+            image: require('../../assets/images/quiz/dez.gif'),
+        },
 
     ]
 }
